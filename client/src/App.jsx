5ba916adf8fb3f88808fee9eb3c7b754fb63fc37
Viswanadhapallivennela19 <165765409+Viswanadhapallivennela19@users.noreply.github.com>
2025-07-17 15:30:04 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://tensorgo-backend-7ei0.onrender.com');
 const config = {
   iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
 };
